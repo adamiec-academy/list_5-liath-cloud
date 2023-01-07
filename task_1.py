e5731a98dc62cfaa57ba903ec0102f4a96471155
liath-cloud <115594751+liath-cloud@users.noreply.github.com>
2023-01-07 00:23:41 +0000

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,2 +1,10 @@
-def longest_word():
-    return
+def longest_word(words):
+    longest = ""
+    length = 0
+
+    for i in range(len(words)):
+        if len(words[i]) > length:
+            length = len(words[i])
+            longest = words[i]
+
+    return longest
