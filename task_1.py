f2abf2002f2373cfc7d1eebad01883624538500d
liath-cloud <115594751+liath-cloud@users.noreply.github.com>
2023-01-09 23:41:18 +0000

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -3,7 +3,7 @@ def longest_word():
     data = []
 
     for line in open("words.txt", encoding="utf-8"):
-        data.append(line.strip('/n'))
+        data.append(line.strip('\n'))
 
     length = 0
     longest = ''
