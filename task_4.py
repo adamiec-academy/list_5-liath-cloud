da621ea77619912cdf2a465548aa6986183c43ce
liath-cloud <115594751+liath-cloud@users.noreply.github.com>
2023-01-10 00:06:14 +0000

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -1,2 +1,19 @@
 def reversed_words():
-    return []
+
+    results = []
+    data = []
+
+    for line in open("words.txt", encoding="utf-8"):
+        data.append(line.strip('\n'))
+
+    data_krotka = set(data)
+
+    for i in range(len(data)):
+        data_inverse = data[i][::-1]
+        if data_inverse in data_krotka and data_inverse != data[i]:
+            results.append((data[i], data_inverse))
+            data_krotka.remove(data_inverse)
+            data_krotka.remove(data[i])
+
+    results = sorted(results)
+    return results
